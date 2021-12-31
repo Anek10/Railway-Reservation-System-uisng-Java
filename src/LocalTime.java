77d2100af5f34ec62854a514a87f45996f76d58c
Anek10 <aneky59@gmail.com>
2021-12-31 10:00:02 +0530

    Initail commit

---
+++ src/LocalTime.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ANEK
+ */
+class LocalTime {
+
+    static LocalTime parse(String time1) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    
+}
